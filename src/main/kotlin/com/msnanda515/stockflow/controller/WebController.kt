14489be8f964a2853adaa45671bf08fcb0f19934
Meharpreet Singh Nanda <msnanda515@gmail.com>
2022-05-14 15:12:24 -0600

    Add demo controller

---
+++ src/main/kotlin/com/msnanda515/stockflow/controller/WebController.kt
@@ -0,0 +1,13 @@
+package com.msnanda515.stockflow.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class WebController {
+
+    @GetMapping("/")
+    fun index(): String {
+        return "index"
+    }
+}
