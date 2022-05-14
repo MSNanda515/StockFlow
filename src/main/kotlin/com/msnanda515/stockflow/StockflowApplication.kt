a274f0ce61b40cf334e07c649bc72771a2347152
Meharpreet Singh Nanda <msnanda515@gmail.com>
2022-05-14 14:59:46 -0600

    Initial commit

---
+++ src/main/kotlin/com/msnanda515/stockflow/StockflowApplication.kt
@@ -0,0 +1,11 @@
+package com.msnanda515.stockflow
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StockflowApplication
+
+fun main(args: Array<String>) {
+	runApplication<StockflowApplication>(*args)
+}
