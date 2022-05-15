95f7bbdcd6b5e3c6f3aa3f10c7e5aae13521342e
Meharpreet Singh Nanda <msnanda515@gmail.com>
2022-05-15 08:32:57 -0600

    Configure workflow for cd wip

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM openjdk:18.0.1.1-oraclelinux7
-WORKDIR /app
-EXPOSE 8080
-COPY ./build/libs/*.jar .
-CMD ["java", "-jar", "stockflow-0.0.1-SNAPSHOT.jar"]
--- build.sh
+++ build.sh
@@ -2,4 +2,7 @@
 ./gradlew build
 
 # Build the docker image and push it to container registry
-docker buildx build --platform linux/amd64,linux/arm64/v8 -t msnanda515/stockflow:latest --push  .
+docker buildx build \
+  --platform linux/amd64,linux/arm64/v8 \
+  -t msnanda515/stockflow:latest \
+  --push -f buildDockerfile .
